a6d2fc6f58ff0466a417a422ebf2d7a9ce569c5a
Leonardo Pereira <contatoleoo@gmail.com>
2025-11-12 20:28:21 -0300

    atualizando arquivos para rodar o mlflow no render

--- src/mlflow_server/app.sh
+++ src/mlflow_server/app.sh
@@ -1,6 +1,13 @@
-#!/bin/bash
+#!/usr/bin/env bash
+echo "Starting MLflow server..."
+
+# Define o diretório onde os dados do MLflow serão armazenados
+export BACKEND_STORE_URI=sqlite:///mlflow.db
+export ARTIFACT_ROOT=./artifacts
+
+# Executa o MLflow
 mlflow server \
-    --backend-store-uri sqlite:///mlflow.db \
-    --default-artifact-root ./mlruns \
+    --backend-store-uri $BACKEND_STORE_URI \
+    --default-artifact-root $ARTIFACT_ROOT \
     --host 0.0.0.0 \
-    --port 5000
+    --port 5002
